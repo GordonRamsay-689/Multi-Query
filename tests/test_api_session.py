c23bf43ce9588e54ff0031c94daae8746a7da40c
Gordon-Ramsay-689 <172498648+GordonRamsay-689@users.noreply.github.com>
2025-02-21 23:35:16 +0100

    Deleted 'TestGeminiFormatResponse'. Moved most tests to 'TestFormatResponse' and some left as comments to be implemented in 'TestGeminiFormatHelperFunctions'. Replaced single qoutes with double qoutes in both classes.

--- tests/test_api_session.py
+++ tests/test_api_session.py
@@ -1,10 +1,12 @@
 # Todo:
-# - Change TestGeminiFormatResponse to simply TestFormatResponse
-# - Add Openai test f_header
+# - Add Openai test f_header and general test_header
 
 from init_tests import append_to_path
+
 append_to_path()
+
 import unittest
+
 import api_session
 import ui
 from constants import * 
@@ -184,17 +186,73 @@ class TestOpenaiClient(unittest.TestCase):
 
 class TestFormatResponse(unittest.TestCase):
 
-    texts = {"code_block_containing_boldened_text": "```python\n**boldened**\n```**boldened**",
-             "inline_code_single": "text`**not bold**` text",
-             "inline_code_multiple": "text`**not bold**` text `*not italic*`",
-             "inline_code_multi_line_single": "text `text \n**bold**`",
-             "inline_code_multi_line_multiple": "text `code \n**bold**`\n`**notbold**`"
+    texts = {   "code_block_containing_boldened_text": "```python\n**boldened**\n```**boldened**",
+                "code_block_multiple": "```python\nCode\n```\nText\n```C++\nCode\n```\nText",
+                "code_block_containing_italicized_text": "```python\nHey, some *code*\n```",
+                "inline_code_single": "text`**not bold**` text",
+                "inline_code_multiple": "text`**not bold**` text `*not italic*`",
+                "inline_code_multi_line_single": "text `text \n**bold**`",
+                "inline_code_multi_line_multiple": "text `code \n**bold**`\n`**notbold**`",
+                "bullet_list_multi_line": '''
+* 1 cup red lentils, rinsed
+* 2 cups water or vegetable broth
+* 1 tbsp oil
+* 1 tsp ground cumin
+* 1 tsp ground coriander
+* ½ tsp turmeric powder
+* ½ tsp garam masala
+* ¼ tsp cayenne pepper (optional)
+* 1 small onion, chopped
+* 2 cloves garlic, minced
+* 1 inch ginger, grated
+* Salt to taste
+* Fresh cilantro, chopped (for garnish)''', 
+                "bullet_list_multi_line_indented": '''1. Test CMake Directly:
+   * Install CMake independently of Homebrew (e.g., from the CMake website's official macOS distribution).  
+   * Create a simple CMake project (e.g., `CMakeLists.txt` containing `cmake_minimum_required(VERSION 3.10) project(MyProject) add_executable(MyProject main.cpp)`, and `main.cpp` containing a basic "Hello, world!" program).
+   * Try to build it using the independently installed CMake from the command line. This helps determine if CMake itself is malfunctioning.''',
+                "bullet_item": "\n* Large pot or Dutch oven",
+                "bullet_item_boldened_text_normal_text": "\n* **Boldened Text** Unboldened text.",
+                "bullet_item_boldened_italic_text": "\n* ***Boldened Italic.***",
+                "bullet_item_boldened_text_asterisks": "\n* * **Boldened NOT Italic.** *",
+                "bullet_item_indented": "\n    * A",
+                "bullet_item_asterisk": "\n* Dial *555",
+                "italic_text_containing_boldened_text": "*Italic **bold-italic** Italic*",
+                "boldened_italic_close": "***Hey***",
              }
-    f_texts = {"code_block_containing_boldened_text": "\tpython: - - - - -\n**boldened**\n\t- - - - - - - - - -\033[39;49;1mboldened\033[22m",
+    f_texts = { "code_block_containing_boldened_text": "\tpython: - - - - -\n**boldened**\n\t- - - - - - - - - -\033[39;49;1mboldened\033[22m",
+                "code_block_multiple": "\tpython: - - - - -\nCode\n\t- - - - - - - - - -\nText\n\tC++: - - - - -\nCode\n\t- - - - - - - - - -\nText",
+                "code_block_containing_italicized_text": "\tpython: - - - - -\nHey, some *code*\n\t- - - - - - - - - -",
                 "inline_code_single": "text`**not bold**` text",
                 "inline_code_multiple": "text`**not bold**` text `*not italic*`",
                 "inline_code_multi_line_single": "text `text \n\033[39;49;1mbold\033[22m`",
-                "inline_code_multi_line_multiple": "text `code \n\033[39;49;1mbold\033[22m`\n`**notbold**`"
+                "inline_code_multi_line_multiple": "text `code \n\033[39;49;1mbold\033[22m`\n`**notbold**`",
+                "bullet_list_multi_line": '''
+- 1 cup red lentils, rinsed
+- 2 cups water or vegetable broth
+- 1 tbsp oil
+- 1 tsp ground cumin
+- 1 tsp ground coriander
+- ½ tsp turmeric powder
+- ½ tsp garam masala
+- ¼ tsp cayenne pepper (optional)
+- 1 small onion, chopped
+- 2 cloves garlic, minced
+- 1 inch ginger, grated
+- Salt to taste
+- Fresh cilantro, chopped (for garnish)''',
+                "bullet_list_multi_line_indented": '''1. Test CMake Directly:
+   - Install CMake independently of Homebrew (e.g., from the CMake website's official macOS distribution).  
+   - Create a simple CMake project (e.g., `CMakeLists.txt` containing `cmake_minimum_required(VERSION 3.10) project(MyProject) add_executable(MyProject main.cpp)`, and `main.cpp` containing a basic "Hello, world!" program).
+   - Try to build it using the independently installed CMake from the command line. This helps determine if CMake itself is malfunctioning.''',
+                "bullet_item": "\n- Large pot or Dutch oven",
+                "bullet_item_boldened_text_normal_text": "\n- \033[39;49;1mBoldened Text\033[22m Unboldened text.",
+                "bullet_item_boldened_italic_text": "\n- \033[39;49;1m\033[39;49;3mBoldened Italic.\033[22m\033[23m",
+                "bullet_item_boldened_text_asterisks": "\n- * \033[39;49;1mBoldened NOT Italic.\033[22m *",
+                "bullet_item_indented": "\n    - A",
+                "bullet_item_asterisk": "\n- Dial *555",
+                "italic_text_containing_boldened_text": "\033[39;49;3mItalic \033[39;49;1mbold-italic\033[22m Italic\033[23m",
+                "boldened_italic_close": "\033[39;49;1m\033[39;49;3mHey\033[22m\033[23m",
                }
 
     expected_failures = []
@@ -215,7 +273,7 @@ class TestFormatResponse(unittest.TestCase):
             self.assertEqual(post, expected)
 
     def test_format_response(self):
-        clients = [GPT_4_ID, GEMINI_FLASH_ID]
+        clients = [GPT_4_ID, GEMINI_FLASH_ID] # One per type
 
         for client_id in clients:
             self.session = api_session.Session(client_id)
@@ -227,233 +285,71 @@ class TestFormatResponse(unittest.TestCase):
 
                     self.compare(pre, expected, key)
     
-# REWRITE AS GENERAL TEST FOR api_session.format_response() (results should not be client specific)
-class TestGeminiFormatResponse(unittest.TestCase):
-
-    ''' Test GeminiClient.format_response(). '''  
-
-    bullet_list = '''
-* 1 cup red lentils, rinsed
-* 2 cups water or vegetable broth
-* 1 tbsp oil
-* 1 tsp ground cumin
-* 1 tsp ground coriander
-* ½ tsp turmeric powder
-* ½ tsp garam masala
-* ¼ tsp cayenne pepper (optional)
-* 1 small onion, chopped
-* 2 cloves garlic, minced
-* 1 inch ginger, grated
-* Salt to taste
-* Fresh cilantro, chopped (for garnish)'''
-    f_bullet_list = '''
-- 1 cup red lentils, rinsed
-- 2 cups water or vegetable broth
-- 1 tbsp oil
-- 1 tsp ground cumin
-- 1 tsp ground coriander
-- ½ tsp turmeric powder
-- ½ tsp garam masala
-- ¼ tsp cayenne pepper (optional)
-- 1 small onion, chopped
-- 2 cloves garlic, minced
-- 1 inch ginger, grated
-- Salt to taste
-- Fresh cilantro, chopped (for garnish)'''
-    
-    indented_bullet_list = '''1. Test CMake Directly:
-   * Install CMake independently of Homebrew (e.g., from the CMake website's official macOS distribution).  
-   * Create a simple CMake project (e.g., `CMakeLists.txt` containing `cmake_minimum_required(VERSION 3.10) project(MyProject) add_executable(MyProject main.cpp)`, and `main.cpp` containing a basic "Hello, world!" program).
-   * Try to build it using the independently installed CMake from the command line. This helps determine if CMake itself is malfunctioning.'''
-    f_indented_bullet_list = '''1. Test CMake Directly:
-   - Install CMake independently of Homebrew (e.g., from the CMake website's official macOS distribution).  
-   - Create a simple CMake project (e.g., `CMakeLists.txt` containing `cmake_minimum_required(VERSION 3.10) project(MyProject) add_executable(MyProject main.cpp)`, and `main.cpp` containing a basic "Hello, world!" program).
-   - Try to build it using the independently installed CMake from the command line. This helps determine if CMake itself is malfunctioning.'''
-
-    @classmethod
-    def setUpClass(cls):
-        cls.session = api_session.Session(GEMINI_FLASH_ID)
-        cls.format_response = cls.session.client.format_response
-
-    @classmethod
-    def tearDownClass(cls):
-        del cls.session
-
-    def func(self, pre):
-        ''' The function we are testing. Returns relevant modified variable '''
-        self.format_response(text=pre)
-        return self.session.client.response
-
-    def compare(self, pre, expected):
-        ''' Compares output of function we are testing when 
-        passed 'pre' with expected output '''
-        post = self.func(pre)
-        #print(repr(post))
-        #ui.c_out(post)
-        return self.assertEqual(post, expected)
-
-    def test_format_response_indented_bullet_list(self):
-        pre = self.indented_bullet_list
-        expected = self.f_indented_bullet_list
-    
-        self.compare(pre, expected)
-
-    def test_format_response_bullet_item(self):
-        pre = '\n* Large pot or Dutch oven'
-        expected = '\n- Large pot or Dutch oven'
-
-        self.compare(pre, expected)
-
-    def test_format_response_bullet_item_followed_by_boldened_text(self):
-        pre = '\n* **Boldened Text** Unboldened text.'
-        expected = '\n- \033[39;49;1mBoldened Text\033[22m Unboldened text.'
-
-        self.compare(pre, expected)
-
-    def test_format_response_bullet_item_followed_by_boldened_italicized_text(self):
-        pre = '\n* ***Boldened Italic.***'
-        expected = '\n- \033[39;49;1m\033[39;49;3mBoldened Italic.\033[22m\033[23m'
-
-        self.compare(pre, expected)
-
-    def test_format_response_bullet_item_followed_by_boldened_text_and_asterisks(self):
-        pre = '\n* * **Boldened NOT Italic.** *'
-        expected = '\n- * \033[39;49;1mBoldened NOT Italic.\033[22m *'
-
-        self.compare(pre, expected)
-
-    def test_format_response_bullet_item_indented(self):
-        pre = '\n    * A'
-        expected = '\n    - A'
-
-        self.compare(pre, expected)
-
-    def test_format_response_bullet_item_complex(self):
-        pre = '* **A'
-        expected = '- A'
-
-        self.compare(pre, expected)
-
-    def test_format_response_bullet_item_followed_by_asterisk(self):
-        pre = '\n* Dial *555'
-        expected = '\n- Dial *555'
-
-        self.compare(pre, expected)
-   
-    def test_format_response_bullet_list(self):
-        ''' Complete multi-line bullet list '''
-        pre = self.bullet_list
-        expected = self.f_bullet_list
-
-        self.compare(pre, expected)
-
-    def test_format_response_code_blocks(self):
-        pre = "```python\nCode\n```\nText\n```C++\nCode\n```\nText"
-        expected = "\tpython: - - - - -\nCode\n\t- - - - - - - - - -\nText\n\tC++: - - - - -\nCode\n\t- - - - - - - - - -\nText"
-
-        self.compare(pre, expected)
-
-    def test_format_response_code_block_containing_numbered_list(self):
-        pre = "```python\n**1. Item\n```\n**2."
-        expected = "\tpython: - - - - -\n**1. Item\n\t- - - - - - - - - -\n2."
-
-        self.compare(pre, expected)
-
-    def test_format_response_code_block_containing_boldened_text(self):
-        pre = "```python\n**boldened**\n```**boldened**"
-        expected = "\tpython: - - - - -\n**boldened**\n\t- - - - - - - - - -\033[39;49;1mboldened\033[22m"
-
-        self.compare(pre, expected)
-
-    def test_format_response_code_block_containing_italicized_text(self):
-        pre = "```python\nHey, some *code*\n```"
-        expected = "\tpython: - - - - -\nHey, some *code*\n\t- - - - - - - - - -"
-
-        self.compare(pre, expected)
-
-    def test_format_response_bold_text_containing_italic_text(self):
-        pre = '*Text **bold** text*'
-        expected = '\033[39;49;3mText \033[39;49;1mbold\033[22m text\033[23m'
-        
-        self.compare(pre, expected)
-
-    def test_format_bold_after_complex_numbered_list_item(self):
-        pre = '* ** **Text**'
-        expected = '- \033[39;49;1mText\033[22m'
-
-    def test_format_response_top_header(self):
-        pre = '\t# This is header'
-        expected = '\t\t This is header'
-
-        self.compare(pre, expected)
-
-    def test_format_response_boldened_italic_close(self):
-        pre = '***Hey***'
-        expected = '\033[39;49;1m\033[39;49;3mHey\033[22m\033[23m'
-
-        self.compare(pre, expected)
-
 class TestGeminiFormatHelperFunctions(unittest.TestCase):
     ''' Tests the formatting methods used by GeminiClient.format_response(). '''
-    
+
+    # Specific to Gemini, insert new f_code_blocks() function.   
+    # "code_block_containing_numbered_list_item": "```python\n**1. Item\n```\n**2.",
+    # "code_block_containing_numbered_list_item": "\tpython: - - - - -\n**1. Item\n\t- - - - - - - - - -\n2.",
+
     texts = {
-        'bullet-complex': '* **Text',
-        'bullet-streamed': '*\nText',
-        'bullet-multiline': '* Text\n* Text',
-        'bullet-multiline-streamed': '*\nText\n*\nText',
-        'bullet-indented-tab': '\t* Text',
-        'bullet-indented-spaces': '\n    * Text',
-        'numbered_lists-item': '**1.',
-        'numbered_lists-list': '**1. Text\n**2. Text',
-        'header-1': '\t# Header 1 #',
-        'header-1-not': '\t # Header',
-        'bold_text-simple': '**Text**',
-        'bold_text-not': '** * Text ***',
-        'simple_math-mult-A': '5 * 10 = 9',
-        'simple_math-mult-B': '5*10 = 9',
-        'simple_math-mult-B': '5*10 * 3 = 9',
-        'simple_math-mult-C': '5*10*3 = 9',
-        'italicized_text-one-char': 'The number *e*, approximately',
-        'italicized_text-asterisks': ' *** ',
-        'italicized_text-sentence-with-asterisks': 'Hey * this is not * italicized',
-        'italicized_text-sentence-with-asterisks-B': 'Hey *this is not * italicized',
-        'italicized_text-sentence': 'Hey *this is* italicized',
-        'italicized_text-sentence-with-asterisk': 'Hey *this is* also* italicized',
-        'italicized_text-asterisk-inside-word': "'*args' text '*args'.",
-        'italicized_text-italicized-asterisk-inside-word': "**args* text '*args'.",
-        'italicized_text-asterisk-in-word-sentence': "`*args`: The `*args` syntax in your `calculate` function definition means *it can accept a variable number of positional arguments*.  These arguments are packed into a tuple named `*args` inside the function."
+        "bullet-complex": "* **Text",
+        "bullet-streamed": "*\nText",
+        "bullet-multiline": "* Text\n* Text",
+        "bullet-multiline-streamed": "*\nText\n*\nText",
+        "bullet-indented-tab": "\t* Text",
+        "bullet-indented-spaces": "\n    * Text",
+        "numbered_lists-item": "**1.",
+        "numbered_lists-list": "**1. Text\n**2. Text",
+        "header-1": "\t# Header 1 #",
+        "header-1-not": "\t # Header",
+        "bold_text-simple": "**Text**",
+        "bold_text-not": "** * Text ***",
+        "simple_math-mult-A": "5 * 10 = 9",
+        "simple_math-mult-B": "5*10 = 9",
+        "simple_math-mult-B": "5*10 * 3 = 9",
+        "simple_math-mult-C": "5*10*3 = 9",
+        "italicized_text-one-char": "The number *e*, approximately",
+        "italicized_text-asterisks": " *** ",
+        "italicized_text-sentence-with-asterisks": "Hey * this is not * italicized",
+        "italicized_text-sentence-with-asterisks-B": "Hey *this is not * italicized",
+        "italicized_text-sentence": "Hey *this is* italicized",
+        "italicized_text-sentence-with-asterisk": "Hey *this is* also* italicized",
+        "italicized_text-asterisk-inside-word": "'*args' text '*args'.",
+        "italicized_text-italicized-asterisk-inside-word": "**args* text '*args'.",
+        "italicized_text-asterisk-in-word-sentence": "`*args`: The `*args` syntax in your `calculate` function definition means *it can accept a variable number of positional arguments*.  These arguments are packed into a tuple named `*args` inside the function."
         }
     f_texts = {
-        'bullet-complex': '- Text',
-        'bullet-streamed': '- Text',
-        'bullet-multiline': '- Text\n- Text',
-        'bullet-multiline-streamed': '- Text\n- Text',
-        'bullet-indented-spaces': '\n    - Text',
-        'bullet-indented-tab': '\t- Text',
-        'numbered_lists-item': '1.',
-        'numbered_lists-list': '1. Text\n2. Text',
-        'header-1': '\t\t Header 1 #',
-        'header-1-not': '\t # Header',
-        'bold_text-simple': '\033[39;49;1mText\033[22m',
-        'bold_text-not': '\033[39;49;1m * Text *\033[22m',
-        'simple_math-mult-A': '5 * 10 = 9',
-        'simple_math-mult-B': '5*10 = 9',
-        'simple_math-mult-B': '5*10 * 3 = 9',
-        'simple_math-mult-C': '5*10*3 = 9',
-        'italicized_text-one-char': 'The number \033[39;49;3me\033[23m, approximately',
-        'italicized_text-asterisks': ' *** ',
-        'italicized_text-sentence-with-asterisks': 'Hey * this is not * italicized',
-        'italicized_text-sentence-with-asterisks-B': 'Hey *this is not * italicized',
-        'italicized_text-sentence': 'Hey \033[39;49;3mthis is\033[23m italicized',
-        'italicized_text-sentence-with-asterisk': 'Hey \033[39;49;3mthis is\033[23m also* italicized',
-        'italicized_text-asterisk-inside-word': "'*args' text '*args'.",
-        'italicized_text-italicized-asterisk-inside-word': "*\033[39;49;3margs\033[23m text '*args'.",
-        'italicized_text-asterisk-in-word-sentence': "`*args`: The `*args` syntax in your `calculate` function definition means \033[39;49;3mit can accept a variable number of positional arguments\033[23m.  These arguments are packed into a tuple named `*args` inside the function."
+        "bullet-complex": "- Text",
+        "bullet-streamed": "- Text",
+        "bullet-multiline": "- Text\n- Text",
+        "bullet-multiline-streamed": "- Text\n- Text",
+        "bullet-indented-spaces": "\n    - Text",
+        "bullet-indented-tab": "\t- Text",
+        "numbered_lists-item": "1.",
+        "numbered_lists-list": "1. Text\n2. Text",
+        "header-1": "\t\t Header 1 #",
+        "header-1-not": "\t # Header",
+        "bold_text-simple": "\033[39;49;1mText\033[22m",
+        "bold_text-not": "\033[39;49;1m * Text *\033[22m",
+        "simple_math-mult-A": "5 * 10 = 9",
+        "simple_math-mult-B": "5*10 = 9",
+        "simple_math-mult-B": "5*10 * 3 = 9",
+        "simple_math-mult-C": "5*10*3 = 9",
+        "italicized_text-one-char": "The number \033[39;49;3me\033[23m, approximately",
+        "italicized_text-asterisks": " *** ",
+        "italicized_text-sentence-with-asterisks": "Hey * this is not * italicized",
+        "italicized_text-sentence-with-asterisks-B": "Hey *this is not * italicized",
+        "italicized_text-sentence": "Hey \033[39;49;3mthis is\033[23m italicized",
+        "italicized_text-sentence-with-asterisk": "Hey \033[39;49;3mthis is\033[23m also* italicized",
+        "italicized_text-asterisk-inside-word": "'*args' text '*args'.",
+        "italicized_text-italicized-asterisk-inside-word": "*\033[39;49;3margs\033[23m text '*args'.",
+        "italicized_text-asterisk-in-word-sentence": "`*args`: The `*args` syntax in your `calculate` function definition means \033[39;49;3mit can accept a variable number of positional arguments\033[23m.  These arguments are packed into a tuple named `*args` inside the function."
         }
 
     expected_failures = [
-        ['f_bold_text', 'bold_text-not'],
-        ['f_italicized_text', 'bold_text-simple'],
+        ["f_bold_text", "bold_text-not"],
+        ["f_italicized_text", "bold_text-simple"],
         ]
 
     @classmethod
